e8284bf86abe6fb09ac8727df5ac09bb6a966324
yhj <yanghengjian@smartcounselor.cn>
2024-06-24 12:15:04 +0800

    更换为两个启动

--- app/test2vec.py
+++ app/test2vec.py
@@ -10,4 +10,4 @@ app.include_router(encode.router)
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=settings["port"])
+    uvicorn.run(app, host='0.0.0.0', port=8001)

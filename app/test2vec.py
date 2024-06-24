0b38ab17363767d68c43f0f7da5ac60d0c280084
yhj <yanghengjian@smartcounselor.cn>
2024-06-24 15:39:28 +0800

    修改线上配置

--- app/test2vec.py
+++ app/test2vec.py
@@ -10,4 +10,4 @@ app.include_router(encode.router)
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8001)
+    uvicorn.run(app, host='0.0.0.0', port=1702)
